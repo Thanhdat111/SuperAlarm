570db0888775d1ebeaa3eb1f98ad25579abde5bb
KT <tunglee01230@gmail.com>
2019-11-24 00:12:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/superalarm/LoginByFaceId.java
@@ -0,0 +1,14 @@
+package com.example.superalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginByFaceId extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_by_face_id);
+    }
+}

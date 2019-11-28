66e979646db400d3db503faf7c9b1e40ed2ab762
KT <tunglee01230@gmail.com>
2019-11-28 12:38:25 +0700

    yes3

--- app/src/main/java/com/example/superalarm/MainActivity.java
+++ app/src/main/java/com/example/superalarm/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.superalarm;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -14,15 +20,54 @@ import com.example.superalarm.UI.Fragment.AlarmFragment;
 import com.example.superalarm.UI.Fragment.QuestionFragment;
 import com.example.superalarm.UI.Fragment.DiaryFragment;
 import com.example.superalarm.UI.Fragment.SettingFragment;
+import com.example.superalarm.UI.Login.PreLoginActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActionBar toolbar;
+    private FragmentManager fragment;
+    private DrawerLayout dl;
+    private NavigationView nav_view;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dl = (DrawerLayout)findViewById(R.id.drawerlayout);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar_mainActivity);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Báo thức");
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        //nav drawer
+        nav_view = (NavigationView)findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                // close drawer when item is tapped
+                dl.closeDrawers();
+
+                //open loginactivity
+                if(id == R.id.sigin_nav){
+                    Intent intent = new Intent(MainActivity.this, PreLoginActivity.class);
+                    startActivity(intent);
+                }else
+                if(id == R.id.logout_nav){
+
+                }
+                return false;
+            }
+        });
 
         //default fragment
         Fragment fragment = new AlarmFragment();
@@ -71,5 +116,14 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                dl.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }

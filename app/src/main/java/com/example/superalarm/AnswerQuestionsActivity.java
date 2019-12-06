59248989ab24770abe9f8c2240192a065044059a
KT <tunglee01230@gmail.com>
2019-12-06 10:49:32 +0700

    xắp xong rồi a đạt ơi

---
+++ app/src/main/java/com/example/superalarm/AnswerQuestionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.superalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnswerQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer_questions);
+    }
+}
